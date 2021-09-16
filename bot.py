939b5fe0850dba7718877f3d720ea96f381c36c8
Dan Z <dan.zaikin@gmail.com>
2021-09-17 08:55:49 +1000

    added search fnc

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 import subprocess
 import asyncio
+import datetime
 
 from discord.ext import commands,tasks
 from dotenv import load_dotenv
@@ -45,39 +46,46 @@ class YoutubeQuery:
         self.url = url
         self.data = None
 
-    async def download_info(self):
-        self.data = await self.loop.run_in_executor(None, lambda: ytdl.extract_info(self.url, download=0))
+    @staticmethod
+    async def download(url, loop, download_full=False):
+        return await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=download_full))
+
+    """
+    async def download(self, download_full=False):
+        self.data = await self.loop.run_in_executor(None, lambda: ytdl.extract_info(self.url, download=download_full))
+    """
 
     async def get_data(self):
         if not self.data:
-            await self.download_info()
+            self.data = await YoutubeQuery.download(self.url, self.loop)
         return self.data
 
-    async def get_songs_data(self):
-        data = await self.get_data()
+    @staticmethod
+    async def get_songs_from_data(data):
         if data['entries']:
             return data['entries']
         else:
             return [data]
 
-    async def get_first_song_title(self):
-        return await self.get_data()[0]['title']
+    async def format_songs(self):
+        data = await self.get_data()
+        songs_data = await YoutubeQuery.get_songs_from_data(data)
+        return '\n'.join(["```"] + [str(ind + 1) + '\t' + str(datetime.timedelta(seconds=datum['duration'])) + '\t' + datum['title'] for ind, datum in enumerate(songs_data)] + ["\n```"])
 
-    async def download_song(self, index):
-        data = await self.loop.run_in_executor(None, lambda: ytdl.extract_info(self.get_songs_data()[index]['webpage-url'], download=1))
-        if 'entries' in data:
-            data = data['entries']
-        return ytdl.prepare_filename(data)
+    async def download_from_list(self, index):
+        data = await self.download(YoutubeQuery.get_songs_from_data(self.get_data())[index]['webpage-url'], download_full=True)
+        return ytdl.prepare_filename(YoutubeQuery.get_songs_from_data(data)[0])
 
 
 async def query_youtube_info(search, loop):
     song = YoutubeQuery(search, loop)
-    return await song.get_songs_data()
+    return await song.get_songs_from_data()
 
 @bot.command(name='getinfo', help='Gets a bit of info about a song')
-async def get_song_info(ctx, *search):
-    data = await query_youtube_info(' '.join(search), bot.loop)
-    await ctx.send("Filename: " + data[0]['title'])
+async def get_song_info(ctx, *, search):
+    songs = YoutubeQuery("ytsearch10: " + search)
+    str = await songs.format_songs()
+    await ctx.send(str)
 
 async def download_youtube(url, loop):
     loop = loop if loop else asyncio.get_event_loop()
