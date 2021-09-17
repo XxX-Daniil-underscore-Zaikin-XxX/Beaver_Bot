00954fa77394e77708521c9cf79499898d0acec3
RichardMarron0 <r3marron@gmail.com>
2021-09-17 12:57:04 +1000

    bbot env

--- bbot/Scripts/activate.bat
+++ bbot/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Richard\Desktop\MemeProgramming\Discord Bot\Beaver_Bot\bbot
+set VIRTUAL_ENV=C:\Users\Richard\Desktop\MemeProgramming\Discord Bot\Beaver_Bot-1\bbot
 
 if not defined PROMPT set PROMPT=$P$G
 
